2ee7eba7fe61ed08a1694c3982ab57cfab30cff1
Husein-mark <alwihusein2807@gmail.com>
2026-04-15 20:58:37 +0700

    lowongan jasa

--- resources/views/Lowongan_Jasa/lowonganjasa.blade.php
+++ resources/views/Lowongan_Jasa/lowonganjasa.blade.php
@@ -4,6 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Vokara Lowongan-Jasa</title>
+
+    <!-- ICON -->
+     <link rel="icon" type="image/png" href="{{ asset('images/fav.png') }}">
     
     {{-- CSS --}}
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
@@ -41,7 +44,12 @@
 <div class="kontener">
     
     <section class="hero" style="background-image: url('{{ asset('images/background.png') }}');">
-        <h1 class="hero-title">Lowongan Jasa</h1>
+    
+    <div class="hero-inner">
+        <div class="hero-top">
+            <h1 class="hero-title">Lowongan Jasa</h1>
+            <button class="post-btn">+ Posting Proyek</button>
+        </div>
 
         <div class="search-box">
             <input type="text" placeholder="Cari Proyek, kategori...">
@@ -49,51 +57,36 @@
 
         <div class="filters">
             <button class="active">Semua</button>
-            <button class="active">Pemula</button>
-            <button class="active">Menengah</button>
-            <button class="active">Mahir</button>
+            <button>Pemula</button>
+            <button>Menengah</button>
+            <button>Mahir</button>
         </div>
+    </div>
+
     </section>
 
     <section class="content">
         <aside class="sidebar">
-        <h3>Kategori</h3>
-        <ul>
-            <li class="active">Semua</li>
-            <li>Menengah</li>
-            <li>Menengah</li>
-            <li>Menengah</li>
-            <li>Menengah</li>
-        </ul>
+            <h3>Kategori</h3>
+            <ul>
+                <li class="active">Semua</li>
+                <li>Menengah</li>
+                <li>Menengah</li>
+                <li>Menengah</li>
+                <li>Menengah</li>
+            </ul>
         </aside>
+
+        <div class="jobs">
+            <div class="card"><span class="job-title">Pemula</span></div>
+            <div class="card"><span class="job-title">Pemula</span></div>
+            <div class="card"><span class="job-title">Semua</span></div>
+            <div class="card"><span class="job-title">Menengah</span></div>
+            <div class="card"><span class="job-title">Menengah</span></div>
+            <div class="card"><span class="job-title">Semua</span></div>
+        </div>
     </section>
-
-    <div class="jobs">
-        <div class="card">
-            <span class="job-title">Pemula</span>
-        </div>
-
-        <div class="card">
-            <span class="job-title">Pemula</span>
-        </div>
-
-        <div class="card">
-            <span class="job-title">Semua</span>
-        </div>
-
-        <div class="card">
-            <span class="job-title">Menengah</span>
-        </div>
-
-        <div class="card">
-            <span class="job-title">Menengah</span>
-        </div>
-
-        <div class="card">
-            <span class="job-title">Semua</span>
-        </div>
-    </div>
-
+</div>
     <footer class="footer">
         <div class="footer-left">
             <h2>Vokara</h2>
@@ -129,7 +122,6 @@
             &copy; 2026 Vokara. Seluruh Hak Cipta Dilindungi Undang-Undang
         </p>
     </footer>
-</div>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.min.js" integrity="sha384-G/EV+4j2dNv+tEPo3++6LCgdCROaejBqfUeNjuKAiuXbjrxilcCdDz6ZAVfHWe1Y" crossorigin="anonymous"></script>
 </body>
