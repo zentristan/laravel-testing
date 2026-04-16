7c46fc36c7ae10f864960522657308597b990fb4
Zentristan vitadi <zentristan@Zentristans-MacBook-Air.local>
2026-04-16 15:28:35 +0700

    hero-Landing

---
+++ public/js/LandingPage.js
@@ -0,0 +1,9 @@
+var typed = new Typed('#typed-text', {
+    strings: ['Kerjakan Project Nyata, Bukan Sekadar Tugas', 'Dari Siswa Jadi Freelancer Siap Kerja',
+        'Bangun Portofolio Nyata Sebelum Lulus'
+    ],
+    typeSpeed: 40,
+    backSpeed: 30,
+    backDelay: 4000,
+    loop: true,
+});
--- resources/views/Landing_Page/index.blade.php
+++ resources/views/Landing_Page/index.blade.php
@@ -28,18 +28,25 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
 
     <link rel="stylesheet" href="{{ asset('css/LandingPage.css') }}">
+    <link rel="icon" type="image/png" href="{{ asset('images/fav.png') }}">
 </head>
 
 <body>
     <nav class="navbar">
         <div class="container">
             <div class="nav-content">
-                <div class="logo1">
-
+                <div class="nav-logo-wrap">
+                    <a href="#" class="logo-link">
+                        <img src="{{ asset('images/LightLogoFull.png') }}" alt="Vokara Logo" class="logo-image">
+                    </a>
                 </div>
-                <div class="nav-menu">
+                <div class="nav-menu d-flex">
                     <a href="#" class="nav-link">Jelajahi Jasa</a>
                     <a href="#" class="nav-link">Cari Talenta</a>
+                </div>
+                <div class="nav-button d-flex">
+                    <a href="#" class="btn-primary">Daftar Gratis</a>
+                    <a href="#" class=btn-secondary>Masuk</a>
                 </div>
                 <div class="hamburger" id="hamburger">
                     <span></span>
@@ -50,21 +57,44 @@
         </div>
     </nav>
 
-    <section class="hero">
+    <section class="hero d-flex align-items-center">
         <div class="container">
-
+            <div class="hero-content">
+                <div class="row">
+                    <div class="col text-center ">
+                        <h1 class="HeroTagline-Text"><span id="typed-text"></span></h1>
+                        <p class="HeroDescription-Text">“Platform untuk siswa SMK mengerjakan project real dari klien & membangun portofolio profesional”</p>
+                        <div class="d-flex justify-content-center gap-3 mt-4">
+                            <a href="#" class="btn-primary fs-5 fw-light">Jelajahi Jasa</a>
+                            <a href="#" class="btn-secondary fs-5 fw-light">Cari Talenta</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </section>
 
     <section class="tentang-kami">
         <div class="container">
-
+            <div class="tentang-kami-content">
+                <div class="row">
+                    <div class="col">
+                        <div>
+                            <h1 class="heading-tentang">TENTANG</h1>
+                            <h3 class="heading-kami">KAMI</h3>
+                        </div>
+                        <div>
+                            <tr></tr>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </section>
 
     <section class="keahlian">
         <div class="container">
-            
+
         </div>
     </section>
 
@@ -80,6 +110,8 @@
         </div>
     </section>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/typed.js@2.1.0/dist/typed.umd.js"></script>
+    <script src="{{ asset('js/LandingPage.js') }}"></script>
 </body>
 
 </html>
