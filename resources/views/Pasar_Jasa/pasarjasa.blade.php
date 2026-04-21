a76966f2fa90573eb8c1ac8ad2e9741e520292c6
Niph19 <130121624+Niph19@users.noreply.github.com>
2026-04-21 12:13:30 +0700

    Card Finish

--- resources/views/Pasar_Jasa/pasarjasa.blade.php
+++ resources/views/Pasar_Jasa/pasarjasa.blade.php
@@ -37,60 +37,86 @@
 </head>
 
 <x-layout>
-<body>
-    <section class="hero-mp">
-    
-    <div class="hero-2">
-        <div class="hero-top">
-            <h1 class="hero-title">Marketplace</h1>
-        </div>
-        <h3 class="hero-subtitle">0 Jasa Ditemukan dari Siswa SMK</h3>
 
-        <div class="search-box-mp">
-            <input type="text" placeholder="Cari Jasa, Kategori...">
-            {{-- <button class="search-filter d-flex"><img src="{{ asset("images/sliders-solid.png") }}" width="25" height="25"><p>Filter</p></button> --}}
-            <button class="search-button-mp"><img src="{{ asset("images/magnifying-glass-solid.png") }}" width="25" height="25"></button>
-        </div>
-    </div>
+    <body>
+        <section class="hero-mp">
 
-    </section>
+            <div class="hero-2">
+                <div class="hero-top">
+                    <h1 class="hero-title">Marketplace</h1>
+                </div>
+                <h3 class="hero-subtitle">0 Jasa Ditemukan dari Siswa SMK</h3>
 
-    <section class="content-mp">
-        <aside class="sidebar-mp">
-            <h3>Kategori</h3>
-            <ul>
-                <li class="outfit active">Semua</li>
-                <li class="outfit ">Desain Grafis</li>
-                <li class="outfit ">Web Development</li>
-                <li class="outfit ">Video Editing</li>
-                <li class="outfit ">Animasi</li>
-                <li class="outfit ">Fotografi</li>
-            </ul>
-        </aside>
-
-        <div class="jobs-mp row row-cols-lg-3 gap-3">
-
-            <div class="col card-mp p-0">
-                <img class="card-img-mp" src="{{ asset("images/card_image.jpg") }}">
-                <span class="card-tag-mp outfit text-color"><img class="me-1" width="15" height="15" src="{{ asset("images/tag-solid.png") }}">Web Development</span>
-                <div class="card-bottom-mp d-flex">
-                        <img class="avatar avatar-sm rounded-circle" src="{{ asset("images/background.png") }}">
-                        <div class="d-flex flex-column">
-                            <p class="card-name-mp outfit text-color mb-0">Nama Siswa</p>
-                            <p class="card-subname-mp outfit mb-0">Keahlian – Asal Sekolah</p>
-                        </div>
+                <div class="search-box-mp">
+                    <input type="text" placeholder="Cari Jasa, Kategori...">
+                    {{-- <button class="search-filter d-flex"><img src="{{ asset(" images/sliders-solid.png") }}"
+                            width="25" height="25">
+                        <p>Filter</p>
+                    </button> --}}
+                    <button class="search-button-mp"><img src="{{ asset("images/magnifying-glass-solid.png") }}"
+                            width="25" height="25"></button>
                 </div>
             </div>
+
+        </section>
+
+        <section class="content-mp">
+            <aside class="sidebar-mp">
+                <h3>Kategori</h3>
+                <ul>
+                    <li class="outfit active">Semua</li>
+                    <li class="outfit ">Desain Grafis</li>
+                    <li class="outfit ">Web Development</li>
+                    <li class="outfit ">Video Editing</li>
+                    <li class="outfit ">Animasi</li>
+                    <li class="outfit ">Fotografi</li>
+                </ul>
+            </aside>
+
+            <div class="jobs-mp row row-cols-lg-3 gap-3">
+
+                <div class="col card-mp p-0">
+                    <img class="card-img-mp" src="{{ asset("images/card_image.jpg") }}">
+                    <span class="card-tag-mp outfit"><img class="me-1" width="15" height="15"
+                            src="{{ asset("images/tag-solid.png") }}">Kategori</span>
+                    <div class="card-bottom-mp d-flex flex-column">
+                        <div class="d-flex">
+                            <img class="avatar avatar-sm rounded-circle" src="{{ asset("images/background.png") }}">
+                            <div class="d-flex flex-column">
+                                <p class="card-name-mp outfit text-color mb-0">Nama Siswa</p>
+                                <p class="card-subname-mp outfit mb-0">Keahlian – Asal Sekolah</p>
+                            </div>
+                        </div>
+                        <p class="card-bottom-title-mp text-color mt-2">Judul Jasa yang Ditawarkan Pada Card.</p>
+                        <div class="card-bottom-info d-flex justify-content-between">
+                            <div class="d-flex gap-2 object-fit-contain mt-3">
+                                <div class="rating d-flex object-fit-contain">
+                                    <img src="{{ asset("images/star-solid.png") }}">
+                                    <p class="outfit mb-0">5.0</p>
+                                </div>
+                                <div class="deadline d-flex object-fit-contain">
+                                    <img src="{{ asset("images/clock-regular.png") }}">
+                                    <p class="outfit">7 Hari</p>
+                                </div>
+                            </div>
+                            <div class="harga d-flex flex-column mt-2">
+                                <p class="harga-title outfit">Mulai dari</p>
+                                <p class="harga-angka outfit">Rp 100.000</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
         </div>
-    </section>
-</div>
 
 </x-layout>
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
-        integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r"
-        crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.min.js"
-        integrity="sha384-G/EV+4j2dNv+tEPo3++6LCgdCROaejBqfUeNjuKAiuXbjrxilcCdDz6ZAVfHWe1Y"
-        crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
+    integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r"
+    crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.min.js"
+    integrity="sha384-G/EV+4j2dNv+tEPo3++6LCgdCROaejBqfUeNjuKAiuXbjrxilcCdDz6ZAVfHWe1Y"
+    crossorigin="anonymous"></script>
 </body>
+
 </html>
