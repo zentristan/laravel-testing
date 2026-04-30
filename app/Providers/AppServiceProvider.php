ed8fcfec583c99cebcc850b0d0efcaab1e9ef0c0
zentristan vitadi <zentristan@gmail.com>
2026-04-30 18:49:14 +0700

    rahhhh 3 kali

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -9,6 +10,8 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Register any application services.
      */
+
+
     public function register(): void
     {
         //
@@ -19,6 +22,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        if (config('app.env') === 'production') {
+            URL::forceScheme('https');
+        }
     }
 }
--- resources/views/components/layout_dashboard.blade.php
+++ resources/views/components/layout_dashboard.blade.php
@@ -28,19 +28,14 @@
     <link rel="stylesheet" href="{{ asset('css/ubah_siswa.css') }}">
     <link rel="stylesheet" href="{{ asset('css/keterangan_siswa.css') }}">
     <link rel="stylesheet" href="{{ asset('css/keterangan_siswa_diterima.css') }}">
-
-    
 </head>
 <body>
 
         <x-sidebar></x-sidebar>
 
-
-
         <main>
             {{ $slot }}
         </main>
-
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
